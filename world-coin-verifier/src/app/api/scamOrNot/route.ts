f6bb90d497b06b80b781e0dda6cff4ebb2d65f96
lazycoder1 <gautamgsabhahit@gmail.com>
2024-09-22 07:08:52 +0800

    updating data for scamOrNot api

--- world-coin-verifier/src/app/api/scamOrNot/route.ts
+++ world-coin-verifier/src/app/api/scamOrNot/route.ts
@@ -20,7 +20,7 @@ const scamOrNott = async (number) => {
     console.log(hashIt(number));
     const flaggedCalls = await fetchFlaggedCalls(hashIt(number));
     if (flaggedCalls.length > 2) {
-        return { scam: true };
+        return { scam: true, score: flaggedCalls.length };
     }
     else return { scam: false };
 }
